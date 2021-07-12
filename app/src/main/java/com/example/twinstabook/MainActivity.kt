44ce848c1044e2de2095842971836d1aad8dfeb0
Aditya Madbhavi <aadim2999@gmail.com>
2021-07-12 15:16:14 +0530

    TwInstaBook App For social media

---
+++ app/src/main/java/com/example/twinstabook/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.twinstabook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import android.provider.VoicemailContract
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.twinstabook.daos.PostDao
+import com.example.twinstabook.models.Post
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.Query
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.common.api.ResultCallback
+import com.google.android.gms.common.api.Status
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), IPostAdapter {
+
+    private lateinit var postDao: PostDao
+    private lateinit var adapter: PostAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener{
+            val intent = Intent(this, CreatePostActivity::class.java)
+            startActivity(intent)
+        }
+
+        LogOutButton.setOnClickListener {
+            // helps user to get the keypad part again
+            AuthUI.getInstance().signOut(this)
+            // new activity opens
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            // call sign out function
+            signOut()
+        }
+
+        setUpRecyclerView()
+    }
+
+    private fun signOut() {
+        // performs signOut from main Activity
+        FirebaseAuth.getInstance().signOut()
+    }
+
+    private fun setUpRecyclerView() {
+        postDao = PostDao()
+        val postsCollections = postDao.postCollections
+        val query = postsCollections.orderBy("createdAt", Query.Direction.DESCENDING)
+        val recyclerViewOptions = FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+
+        adapter = PostAdapter(recyclerViewOptions, this)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
+    override fun onLikeClicked(postId: String) {
+        postDao.updateLikes(postId)
+    }
+}
